a600e4101a57a2de1d82c407f65daf29a356abd0
AmondeBong <oms1218ju@gmail.com>
2022-11-01 15:05:56 +0900

    22.11.01 hubo

--- hubo/hubo.py
+++ hubo/hubo.py
@@ -1,4 +1,16 @@
 from cs1robots import *
 create_world()
 hubo = Robot()
+
+
+def turn_right():
+    for i in range(3):
+        hubo.turn_left()
+
+
 hubo.set_trace("red")
+hubo.move()
+hubo.turn_left()
+hubo.move()
+turn_right()
+hubo.move()
